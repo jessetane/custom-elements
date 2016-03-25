458d96a38d40cdf732986cd016551685f5e33328
Jesse Tane <jesse.tane@gmail.com>
2016-03-24 21:23:22 -0400

    test attributeChangedCallback

--- test/index.js
+++ test/index.js
@@ -17,6 +17,40 @@ tape('attached and detacted callbacks are called synchronously', function (t) {
   t.equal(thing.innerHTML, '0')
 })
 
+tape('attribute changed callback works', function (t) {
+  t.plan(9)
+  var thing = document.createElement('x-thing')
+  document.body.appendChild(thing)
+  step1()
+  function step1 () {
+    thing.attributeChangedCallback = function (name, oldValue, newValue) {
+      t.equal(name, 'data-testing')
+      t.equal(oldValue, null)
+      t.equal(newValue, 'much')
+      step2()
+    }
+    thing.setAttribute('data-testing', 'much')
+  }
+  function step2 () {
+    thing.attributeChangedCallback = function (name, oldValue, newValue) {
+      t.equal(name, 'data-testing')
+      t.equal(oldValue, 'much')
+      t.equal(newValue, 'wow')
+      step3()
+    }
+    thing.setAttribute('data-testing', 'wow')
+  }
+  function step3 () {
+    thing.attributeChangedCallback = function (name, oldValue, newValue) {
+      t.equal(name, 'data-testing')
+      t.equal(oldValue, 'wow')
+      t.equal(newValue, null)
+      thing.remove()
+    }
+    thing.removeAttribute('data-testing')
+  }
+})
+
 tape('innerHTML works (asynchronously)', function (t) {
   t.plan(1)
   var wrapper = document.createElement('div')
